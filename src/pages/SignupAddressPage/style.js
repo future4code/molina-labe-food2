41ab2e5eeed5cd5abeb3eef686d34d086b4cfe53
joaopedrocoder <joaopedrolr.7@gmail.com>
2021-07-29 16:43:43 -0300

    desenvolvidas as funcionalidades da pagina de cadastrar endereço

---
+++ src/pages/SignupAddressPage/style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const MainContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    p {
+        font-size: 1.125em;
+        letter-spacing: -0.39px;
+        margin-bottom: 12px;
+    }
+  
+    a{
+        text-decoration: none;
+    }
+`;
+
+export {MainContainer}
