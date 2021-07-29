4c587e43755745f2388f285b83a65b37c2521811
Ishmaiha <ishmaihahkim@gmail.com>
2021-07-29 15:52:18 -0300

    profilepage agora sim completo na parte funcional

--- src/router/Route.js
+++ src/router/Route.js
@@ -43,7 +43,6 @@ const Router = () => {
                     <UpdateAdressPage/>
                 </Route>
 
-
                 <Route exact path={'/restaurante/:restauranteId'}>
                     <OrderPage/>
                 </Route>
